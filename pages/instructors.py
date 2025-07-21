1e1f2cf81694f5e17fca21abb6485233c0ec118e
Maneesha Sane <ahseenam@gmail.com>
2025-07-21 13:33:44 -0400

    display tables on pages

--- pages/instructors.py
+++ pages/instructors.py
@@ -1,13 +1,25 @@
 import dash
-from dash import html, dcc, callback, Input, Output
+from dash import html, dash_table
+from utils.build_pages import get_json_from_url 
+import os 
+
+redash_api_key = os.getenv("REDASH_KEY_QUERY776")
+
+redash_data_url = f"http://redash.carpentries.org/api/queries/776/results.json?api_key={redash_api_key}"
+
+df = get_json_from_url(redash_data_url)
 
 dash.register_page(__name__)
 
 layout = html.Div([
     html.H1('These are our Instructors'),
-    html.Div("Some text about the instructors. They teach our workshops."),
+    html.Div("Something about Instructors. They teach DC, LC, SWC workshops."),
     html.Br(),
     html.Div(id='analytics-output'),
+        # Display table
+    dash_table.DataTable(
+        data=df,  # Pass the list of dictionaries as data
+    ),
+
 
 ]) # close outer html.Div 
-
--- pages/trainers.py
+++ pages/trainers.py
@@ -1,5 +1,13 @@
 import dash
-from dash import html, dcc, callback, Input, Output
+from dash import html, dash_table
+from utils.build_pages import get_json_from_url 
+import os 
+
+redash_api_key = os.getenv("REDASH_KEY_QUERY523")
+
+redash_data_url = f"http://redash.carpentries.org/api/queries/523/results.json?api_key={redash_api_key}"
+
+df = get_json_from_url(redash_data_url)
 
 dash.register_page(__name__)
 
@@ -8,5 +16,10 @@ layout = html.Div([
     html.Div("Some text about the Trainers. They teach our future Instructors."),
     html.Br(),
     html.Div(id='analytics-output'),
+        # Display table
+    dash_table.DataTable(
+        data=df,  # Pass the list of dictionaries as data
+    ),
+
 
 ]) # close outer html.Div 

8863ad036a8bc89c72bf89f94eb21e7d03985671
Maneesha Sane <ahseenam@gmail.com>
2025-07-21 13:38:56 -0400

    include json & pd df data types

--- pages/trainers.py
+++ pages/trainers.py
@@ -2,12 +2,14 @@ import dash
 from dash import html, dash_table
 from utils.build_pages import get_json_from_url 
 import os 
+import pandas as pd 
 
 redash_api_key = os.getenv("REDASH_KEY_QUERY523")
 
 redash_data_url = f"http://redash.carpentries.org/api/queries/523/results.json?api_key={redash_api_key}"
 
-df = get_json_from_url(redash_data_url)
+trainers_json = get_json_from_url(redash_data_url)
+trainers_df = pd.DataFrame(trainers_json)
 
 dash.register_page(__name__)
 
@@ -18,7 +20,7 @@ layout = html.Div([
     html.Div(id='analytics-output'),
         # Display table
     dash_table.DataTable(
-        data=df,  # Pass the list of dictionaries as data
+        data=trainers_json,  # Pass the list of dictionaries as data
     ),
 
 

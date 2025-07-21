05c40746c591b05ad61c5679df90183f52e14003
Maneesha Sane <ahseenam@gmail.com>
2025-07-21 14:50:39 -0400

    misc changes to app page

--- app.py
+++ app.py
@@ -3,13 +3,13 @@ from dash import Dash, html, dcc, page_container, page_registry
 app = Dash(__name__, use_pages=True)
 
 app.layout = html.Div([
-    html.H1('Multi-page app with Dash Pages'),
+    html.H1('The Carpentries Dashboards'),
     html.Div([
         html.Div(
-            dcc.Link(f"{page['name']} - {page['path']}", href=page["relative_path"])
+            dcc.Link(f"{page['name']}", href=page["relative_path"])
         ) for page in page_registry.values()
     ]),
-    page_container
+    page_container,
 ])
 
 if __name__ == '__main__':

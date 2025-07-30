50e6625389fcc51c3dc1ad0ec2384be9a0a2eb3a
Maneesha Sane <ahseenam@gmail.com>
2025-07-30 15:57:53 -0400

    comment out old code

--- pages/instructors.py
+++ pages/instructors.py
@@ -4,35 +4,35 @@ from utils.build_pages import get_json_from_query_number,create_country_bar_char
 import pandas as pd
 
 # Initialize page 
-dash.register_page(__name__, title="Instructors")
+# dash.register_page(__name__, title="Instructors")
 
-# Load in instructors data as json and df 
-instructors_json = get_json_from_query_number(776)
-instructors_df = pd.DataFrame(instructors_json)
+# # Load in instructors data as json and df 
+# instructors_json = get_json_from_query_number(776)
+# instructors_df = pd.DataFrame(instructors_json)
 
-# Create country counts df 
-instructors_country_counts_df = instructors_df['country'].value_counts().reset_index()
+# # Create country counts df 
+# instructors_country_counts_df = instructors_df['country'].value_counts().reset_index()
 
-# Create country counts bar chart 
-chart = create_country_bar_chart(instructors_country_counts_df)
+# # Create country counts bar chart 
+# chart = create_country_bar_chart(instructors_country_counts_df)
 
-# Set up page layout
-layout = html.Div([
-    # Page heading
-    html.H1('These are our Instructors'),
-    # Page intro text 
-    html.Div("Something about Instructors. They teach DC, LC, SWC workshops."),
-    html.Br(),
-    # Display table
-    dash_table.DataTable(
-        data=instructors_json, 
-        # Add sort feature to table
-        sort_action='native',
-        # Set number of rows to display
-        page_size=20,
-    ),
-    # Display bar plot
-    html.H2('Plot Instructors by country'),
-    dcc.Graph(figure=chart)
+# # Set up page layout
+# layout = html.Div([
+#     # Page heading
+#     html.H1('These are our Instructors'),
+#     # Page intro text 
+#     html.Div("Something about Instructors. They teach DC, LC, SWC workshops."),
+#     html.Br(),
+#     # Display table
+#     dash_table.DataTable(
+#         data=instructors_json, 
+#         # Add sort feature to table
+#         sort_action='native',
+#         # Set number of rows to display
+#         page_size=20,
+#     ),
+#     # Display bar plot
+#     html.H2('Plot Instructors by country'),
+#     dcc.Graph(figure=chart)
 
-]) # close outer html.Div 
+# ]) # close outer html.Div 
--- pages/trainers.py
+++ pages/trainers.py
@@ -4,35 +4,35 @@ from utils.build_pages import get_json_from_query_number, create_country_bar_cha
 import pandas as pd 
 
 # Initialize page 
-dash.register_page(__name__, title="Trainers")
+# dash.register_page(__name__, title="Trainers")
 
-# Load in trainers data as json and df 
-trainers_json = get_json_from_query_number(775)
-trainers_df = pd.DataFrame(trainers_json)
+# # Load in trainers data as json and df 
+# trainers_json = get_json_from_query_number(775)
+# trainers_df = pd.DataFrame(trainers_json)
 
-# Create country counts df 
-trainers_country_counts_df = trainers_df['country'].value_counts().reset_index()
+# # Create country counts df 
+# trainers_country_counts_df = trainers_df['country'].value_counts().reset_index()
 
-# Create country counts bar chart 
-chart = create_country_bar_chart(trainers_country_counts_df)
+# # Create country counts bar chart 
+# chart = create_country_bar_chart(trainers_country_counts_df)
 
-# Set up page layout
-layout = html.Div([
-    # Page heading
-    html.H1('These are our Trainers'),
-    # Page intro text
-    html.Div("Some text about the Trainers. They teach our future Instructors."),
-    html.Br(),
-    # Display table
-    dash_table.DataTable(
-        data=trainers_json, 
-        # Add sort feature to table
-        sort_action='native',
-        # Set number of rows to display
-        page_size=20,
-    ),
-    # Display bar plot
-    html.H2('Plot Trainers by country'),
-    dcc.Graph(figure=chart)
+# # Set up page layout
+# layout = html.Div([
+#     # Page heading
+#     html.H1('These are our Trainers'),
+#     # Page intro text
+#     html.Div("Some text about the Trainers. They teach our future Instructors."),
+#     html.Br(),
+#     # Display table
+#     dash_table.DataTable(
+#         data=trainers_json, 
+#         # Add sort feature to table
+#         sort_action='native',
+#         # Set number of rows to display
+#         page_size=20,
+#     ),
+#     # Display bar plot
+#     html.H2('Plot Trainers by country'),
+#     dcc.Graph(figure=chart)
 
-]) # close outer html.Div 
+# ]) # close outer html.Div 

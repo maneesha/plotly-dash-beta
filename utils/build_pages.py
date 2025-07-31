1c811d311665f9602838182988c6942b58cbbe9f
Maneesha Sane <ahseenam@gmail.com>
2025-07-31 10:25:16 -0400

    create function to generate linear & log maps

--- utils/build_pages.py
+++ utils/build_pages.py
@@ -4,6 +4,7 @@ import os
 import pycountry
 import numpy as np
 
+
 def get_json_from_query_number(query_number):
     redash_api_key = os.getenv(f"REDASH_KEY_QUERY{query_number}")
     redash_data_url = f"http://redash.carpentries.org/api/queries/{query_number}/results.json?api_key={redash_api_key}"
@@ -36,7 +37,6 @@ def create_country_bar_chart(df, scale_type):
     elif scale_type == 'log':
         tick_vals = [1, 10, 50, 100, 500, 1000, 2000, 5000]
 
-
     # Update yticks.  Auto or array to use above defined lists
     fig.update_layout(
         yaxis=dict(
@@ -50,7 +50,6 @@ def create_country_bar_chart(df, scale_type):
     return fig 
 
 
-
 # Function to get full country name from alpha-2 code
 def get_country_name(code):
     try:
@@ -58,12 +57,14 @@ def get_country_name(code):
     except:
         return "Country unknown"
 
+
 # Function to get alpha-3 code from alpha-2 code
 def get_country_alpha3(code):
     try:
         return pycountry.countries.get(alpha_2=code).alpha_3
     except:
         return "Country unknown"
+
 
 # Function to create country counts dataframe 
 def get_country_counts_df(df):
@@ -79,3 +80,33 @@ def add_hover_text(df):
     return df
 
 
+def create_country_counts_map(df, scale_type='linear'):
+    """
+    Create a choropleth map of country counts.
+    """
+
+    if scale_type == 'linear':
+        z = df['count']
+        tickvals = [100, 500, 1000, 1500, 2000]
+        ticktext = [100, 500, 1000, 1500, 2000]
+    elif scale_type == 'log':
+        z = np.log10(df['count'])
+        tickvals = [np.log10(v) for v in [25, 50, 100, 300, 1000, 2000]]
+        ticktext = ['25', '50', '100', '300', '1000', '2000']
+
+    # Create choropleth map
+    fig = go.Figure(data=go.Choropleth(
+        locations=df['country_alpha3'],
+        z=z,
+        colorscale='Blues',
+        colorbar=dict(title='Value', tickvals=tickvals, ticktext=ticktext),
+        autocolorscale=False,
+        marker_line_color='darkgray',
+        marker_line_width=0.5,
+        text=df['hover_text'],
+        hovertemplate='%{text}<extra></extra>',
+    ))
+
+    fig.update_geos(projection_type='natural earth', landcolor="LightGray",)
+
+    return fig

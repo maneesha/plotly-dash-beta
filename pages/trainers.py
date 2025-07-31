343c6d4160c51504f4be637a69dc99ced0c8b1b3
Maneesha Sane <ahseenam@gmail.com>
2025-07-31 16:01:11 -0400

    start building out trainers page

--- pages/trainers.py
+++ pages/trainers.py
@@ -1,38 +1,32 @@
 import dash
-from dash import html, dash_table, dcc 
-from utils.build_pages import get_json_from_query_number, create_country_bar_chart  
-import pandas as pd 
+from dash import html, dash_table, dcc, Input, Output, State 
+from utils.build_pages import get_json_from_query_number, create_country_bar_chart,  get_country_counts_df, add_hover_text, create_country_counts_map, create_main_table, set_up_search_filter, create_country_counts_table 
+import pandas as pd
+
 
 # Initialize page 
-# dash.register_page(__name__, title="Trainers")
+dash.register_page(__name__, title="Instructor Trainers")
 
-# # Load in trainers data as json and df 
-# trainers_json = get_json_from_query_number(775)
-# trainers_df = pd.DataFrame(trainers_json)
+# Load in instructor trainers data as json and df 
+trainers_json = get_json_from_query_number(775)
+trainers_df = pd.DataFrame(trainers_json)
 
-# # Create country counts df 
-# trainers_country_counts_df = trainers_df['country'].value_counts().reset_index()
+# Create country counts df 
+# Include column for hover text
+trainers_country_counts_df = get_country_counts_df(trainers_df)
+trainers_country_counts_df = add_hover_text(trainers_country_counts_df)
 
-# # Create country counts bar chart 
-# chart = create_country_bar_chart(trainers_country_counts_df)
+full_table = create_main_table(trainers_df, "trainers-table", 20)
 
-# # Set up page layout
-# layout = html.Div([
-#     # Page heading
-#     html.H1('These are our Trainers'),
-#     # Page intro text
-#     html.Div("Some text about the Trainers. They teach our future Instructors."),
-#     html.Br(),
-#     # Display table
-#     dash_table.DataTable(
-#         data=trainers_json, 
-#         # Add sort feature to table
-#         sort_action='native',
-#         # Set number of rows to display
-#         page_size=20,
-#     ),
-#     # Display bar plot
-#     html.H2('Plot Trainers by country'),
-#     dcc.Graph(figure=chart)
+country_count_header = html.H2('Count Trainers by Country')
+country_count_table =  create_country_counts_table(trainers_country_counts_df, 15)
 
-# ]) # close outer html.Div 
+# Create country counts map
+countries_map_linear = create_country_counts_map(trainers_country_counts_df, scale_type='linear')
+countries_map_log = create_country_counts_map(trainers_country_counts_df, scale_type='log')
+
+log_map_header = html.H2('Map workshops by country - Log Scale')
+log_map = dcc.Graph(figure=countries_map_log,  style={'height': '700px', 'width': '100%'} )
+
+# Set up page layout
+layout = html.Div(["hello world", full_table, country_count_table, log_map ])
